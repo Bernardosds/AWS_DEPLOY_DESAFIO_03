82b51f87edbb81e9ac10ec5b6cac6c977e9342af
Ricardo Amorim <ricardo.contato14@outlok.com>
2024-10-09 20:34:42 -0300

    chore(config): update eslint, package.json, and tsconfig settings

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import 'reflect-metadata';
+import express from 'express';
+import cors from 'cors';
+import AppDataSource from './db/data-source';
+import 'dotenv/config';
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+//app.use(customerRouter);
+
+AppDataSource.initialize().then(async () => {
+  console.log('Conectou ao banco :)');
+  app.listen(process.env.PORT, () => {
+    console.log('Server started');
+  });
+});
