65c709914122d6eec9f4720b5756f9208c964e9a
Ricardo Amorim <ricardo.contato14@outlok.com>
2024-10-09 20:32:37 -0300

    feat(entities): create Customer entity

---
+++ src/modules/customers/entities/Customer.ts
@@ -0,0 +1,36 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('customers')
+class Customer {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', length: 100 })
+  fullName: string;
+
+  @Column({ type: 'date' })
+  birthDate: Date;
+
+  @Column({ type: 'varchar', length: 14 })
+  cpf: string;
+
+  @Column({ type: 'varchar', length: 100 })
+  email: string;
+
+  @Column({ type: 'varchar', length: 20 })
+  phone: string;
+
+  @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp', nullable: true })
+  deletedAt?: Date;
+}
+
+export default Customer;
