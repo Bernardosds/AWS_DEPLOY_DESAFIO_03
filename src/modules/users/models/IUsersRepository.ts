01bf09d9f44f147600201aac33e543685c75b07b
carolgs <goncalves.macarolina@gmail.com>
2024-10-09 21:18:58 -0300

    fix: add 'findByEmail' method to users repository interface

--- src/modules/users/models/IUsersRepository.ts
+++ src/modules/users/models/IUsersRepository.ts
@@ -2,6 +2,7 @@ import IUser from './IUser';
 
 export default interface IUsersRepository {
     create(user: IUser): Promise<string>;
+    findByEmail(email: string): Promise<IUser | null>;
     findById(id: string): Promise<IUser | null>;
     findByName(name: string): Promise<IUser | null>;
     findAll(): Promise<IUser[]>;
