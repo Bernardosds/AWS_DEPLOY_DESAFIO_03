3bb913276b41fbe7679642e7998665a30a324130
carolgs <goncalves.macarolina@gmail.com>
2024-10-09 21:24:30 -0300

    fix: implements 'findByEmail' in the users repository

--- src/modules/users/repositories/UsersRepository.ts
+++ src/modules/users/repositories/UsersRepository.ts
@@ -3,57 +3,65 @@ import User from "../entities/User";
 import IUsersRepository from "../models/IUsersRepository";
 import IUser from "../models/IUser";
 
-export default class UsersRepository implements IUsersRepository{
-    private usersRepository: Repository<User>;
+export default class UsersRepository implements IUsersRepository {
+  private usersRepository: Repository<User>;
 
-    constructor(usersRepository: Repository<User>) {
-        this.usersRepository = usersRepository;
+  constructor(usersRepository: Repository<User>) {
+    this.usersRepository = usersRepository;
+  }
+
+  public async create(user: IUser): Promise<string> {
+    const newUser = await this.usersRepository.create(user);
+    await this.usersRepository.save(newUser);
+
+    return newUser.id as string;
+  }
+
+  public async findByEmail(email: string): Promise<IUser | null> {
+    const user = await this.usersRepository.findOne({
+        where: { email },
+    });
+
+    return user;
+  }
+
+  public async findById(id: string): Promise<IUser | null> {
+    const user = await this.usersRepository.findOne({
+      where: { id },
+    });
+
+    return user || null;
+  }
+
+  public async findByName(name: string): Promise<IUser | null> {
+    const user = await this.usersRepository.findOne({
+      where: { name },
+    });
+
+    return user || null;
+  }
+
+  public async findAll(): Promise<IUser[]> {
+    return await this.usersRepository.find();
+  }
+
+  public async update(id: string, user: Partial<IUser>): Promise<IUser | null> {
+    const result = await this.usersRepository.update(id, user);
+
+    if (result.affected === 0) {
+      return null;
     }
 
-    public async create( user: IUser): Promise<string> {
-        const newUser = await this.usersRepository.create(user);
-        await this.usersRepository.save(newUser);
+    return await this.findById(id);
+  }
 
-        return newUser.id as string;
-    };
+  public async delete(id: string): Promise<void> {
+    const user = await this.findById(id);
 
-    public async findById(id: string): Promise<IUser | null> {
-        const user = await this.usersRepository.findOne({
-            where: { id }
-        });
-
-        return user || null;
+    if (!user) {
+      throw new Error('User not found');
     }
 
-    public async findByName(name: string): Promise<IUser | null> {
-        const user = await this.usersRepository.findOne({
-            where: { name }
-        });
-
-        return user || null;
-    }
-
-    public async findAll(): Promise<IUser[]> {
-        return await this.usersRepository.find();    
-    }
-
-    public async update(id: string, user: Partial<IUser>): Promise<IUser | null> {
-        const result = await this.usersRepository.update(id, user);
-
-        if (result.affected === 0) {
-            return null;
-        }
-
-        return await this.findById(id);
-    }
-
-    public async delete(id: string): Promise<void> {
-        const user = await this.findById(id);
-    
-        if (!user) {
-            throw new Error('User not found');
-        }
-    
-        await this.usersRepository.softRemove(user);    
-    }
+    await this.usersRepository.softRemove(user);
+  }
 }
