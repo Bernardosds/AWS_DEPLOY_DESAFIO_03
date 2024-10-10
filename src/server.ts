adf1b1b5c4ea9ea6bd25f6d96630b4a6c0d8017c
carolgs <goncalves.macarolina@gmail.com>
2024-10-10 19:36:10 -0300

    feat: add app error class, and implements a middleware for handling error responses

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 import cors from 'cors';
-import AppDataSource from './db/data-source';
-import 'dotenv/config';
-import customerRouter from './modules/customers/routes/CustomerRoute';
+import { AppDataSource } from './db/DataSource';
+import userRouter from './modules/users/routes/UsersRoutes';
+import { errors } from 'celebrate';
+import AppError from './shared/errors/AppError';
 
 const app = express();
 
@@ -11,11 +13,36 @@ app.use(cors());
 
 app.use(express.json());
 
-app.use(customerRouter);
+app.use('/users', userRouter);
 
-AppDataSource.initialize().then(async () => {
-  console.log('Conectou ao banco :)');
-  app.listen(process.env.PORT, () => {
-    console.log('Server started');
+app.use(errors());
+
+app.use(
+  (error: unknown, req: Request, res: Response, next: NextFunction): void => {
+    if (error instanceof AppError) {
+      res.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+
+    res.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+
+    return next(error);
+  },
+);
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log('Connected to the database');
+
+    app.listen(process.env.PORT, () => {
+      console.log(`Server started on port ${process.env.PORT}`);
+    });
+  })
+  .catch(error => {
+    console.error('Error connecting to the database:', error);
   });
-});
