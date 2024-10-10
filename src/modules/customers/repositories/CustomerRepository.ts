7e14aab71c7a8996cdba53bcdb7d3ebf5db07a17
Ricardo Amorim <ricardo.contato14@outlok.com>
2024-10-10 12:18:10 -0300

    refactor(customers): simplify the code in CustomerRepository and customer.controllers

--- src/modules/customers/repositories/CustomerRepository.ts
+++ src/modules/customers/repositories/CustomerRepository.ts
@@ -2,21 +2,15 @@ import Customer from '../entities/Customer';
 import ICustomer from '../interfaces/ICustomer';
 import AppDataSource from '../../../db/data-source';
 
-const customerRepository = AppDataSource.getRepository(Customer);
+export const customerRepositorySource = AppDataSource.getRepository(Customer);
 
 class CustomerRepository {
-  createCustomer = async (dataCustomer: ICustomer): Promise<ICustomer> => {
-    const newCustomer = customerRepository.create(dataCustomer);
-    const saveCustomer = await customerRepository.save(newCustomer);
-    return saveCustomer;
-  };
-
   async findCustomerByCpf(cpf: string): Promise<ICustomer | null> {
-    return await customerRepository.findOne({ where: { cpf } });
+    return await customerRepositorySource.findOne({ where: { cpf } });
   }
 
   async findCustomerByEmail(email: string): Promise<ICustomer | null> {
-    return await customerRepository.findOne({ where: { email } });
+    return await customerRepositorySource.findOne({ where: { email } });
   }
 }
 
