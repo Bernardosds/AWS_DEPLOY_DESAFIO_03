a07c997bdd84ce7ca1e0788a997e0119aa22620a
Bernardo Silveira <bernardosdev@gmail.com>
2024-10-11 20:48:40 -0300

    Feat(services): add index.ts to services folder to manage services export

---
+++ src/modules/cars/services/index.ts
@@ -0,0 +1,13 @@
+import CreateCarService from './createCarService';
+import ShowCarService from './showCarService';
+import ListCarsService from './listCarsService';
+import UpdateCarService from './updateCarService';
+import DeleteCarService from './deleteCarService';
+
+export default {
+  CreateCarService,
+  ShowCarService,
+  ListCarsService,
+  UpdateCarService,
+  DeleteCarService,
+};
