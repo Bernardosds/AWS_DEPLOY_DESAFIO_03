053cef6e1b5e3177730d6b0b3734e5037a825538
Bernardo Silveira <bernardosdev@gmail.com>
2024-10-11 20:46:21 -0300

    Feat(services): add UpdateCarService file to services folder

---
+++ src/modules/cars/services/UpdateCarService.ts
@@ -0,0 +1,50 @@
+import Cars from '../entities/Cars';
+import AppDataSource from '../../../db/data-source';
+import ICars from '../interface/ICars';
+import { formatPlate } from './formatters';
+
+class UpdateCarService {
+  updateCar = async (id: string, updates: Partial<ICars>) => {
+    const carsRepository = AppDataSource.getRepository(Cars);
+    const car = await carsRepository.findOneBy({ id });
+
+    if (!car) {
+      throw new Error('Car not found');
+    }
+
+    const updateFields = [
+      'plate',
+      'brand',
+      'model',
+      'mileage',
+      'year',
+      'items',
+      'daily_price',
+    ] as const;
+
+    for (let i = 0; i < updateFields.length; i++) {
+      const field = updateFields[i];
+      if (updates[field] !== undefined) {
+        if (field === 'plate') {
+          car.plate = formatPlate(updates.plate!);
+        } else if (
+          field === 'mileage' ||
+          field === 'year' ||
+          field === 'daily_price'
+        ) {
+          car[field] = updates[field] as number;
+        } else if (field === 'items') {
+          car.items = JSON.stringify(updates.items).trim().toLowerCase();
+        } else {
+          car[field] = updates[field]!.trim();
+        }
+      }
+    }
+
+    await carsRepository.save(car);
+
+    return car;
+  };
+}
+
+export default UpdateCarService;
